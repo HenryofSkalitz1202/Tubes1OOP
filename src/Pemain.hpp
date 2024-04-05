cba897a828bed0d6e3cb07e89ef3ae15217f1d9e
HenryofSkalitz1202 <edarrell1202@gmail.com>
2024-04-05 19:12:19 +0700

    feat: update Pemain

--- src/Pemain.hpp
+++ src/Pemain.hpp
@@ -1,1 +1,95 @@
+#ifndef PEMAIN_HPP
+#define PEMAIN_HPP
+#include <string>
 
+using namespace std;
+
+class Pemain{
+protected:
+  string username;
+  int uang;
+  int beratBadan;
+  string status;
+  //static map<Barang> inventory;
+public:
+  static int beratWin;
+  static int uangWin;
+  static int defaultBerat;
+  static int defaultUang;
+
+  virtual void setStatus() = 0;
+  void setUang();
+  void setString();
+  void setberatBadan();
+  string getStatus();
+  string getUsername();
+  int getUang();
+  int getBeratBadan();
+};
+
+class Petani : public Pemain{
+private:
+  int petaniID;
+  //static map<Tumbuhan> ladang;
+public:
+  Petani();
+  Petani(string username);
+  ~Petani();
+  Petani(Petani& other);
+  Petani& operator=(const Petani& other);
+
+  void setStatus();
+  void setPetaniID();
+  //void setLadang();
+  int getPetaniID();
+  //map<Tumbuhan> getLadang();
+
+  void tanamTanaman();
+  void panenTanaman();
+  void beliBangunan();
+};
+
+class Walikota : public Pemain{
+private:
+  int walikotaID;
+
+public:
+  Walikota();
+  Walikota(string username);
+  ~Walikota();
+  Walikota(Walikota& other);
+  Walikota& operator=(const Walikota& other);
+
+  void setStatus();
+  void setWalikotaID();
+  string getWalikotaID();
+
+  void tagihPajak();
+  void bangunBangunan();
+  void tambahAkun();
+  void jualBangunan();
+};
+
+class Peternak : public Pemain{
+private:
+  int peternakID;
+  //static map<Hewan> peternakan;
+public:
+  Peternak();
+  Peternak(string username);
+  ~Peternak();
+  Peternak(Peternak& other);
+  Peternak& operator=(const Peternak& other);
+
+  void setStatus();
+  void setPeternakID();
+  //void setLadang();
+  int getPeternakID();
+  //map<Tumbuhan> getLadang();
+
+  void taruhHewan();
+  void beriMakan();
+  void panenHewan();
+  void beliBangunan();
+};
+#endif

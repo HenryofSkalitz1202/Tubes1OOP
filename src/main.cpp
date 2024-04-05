bdd3c0f4559dde4c53c127729690106ca6865236
HenryofSkalitz1202 <edarrell1202@gmail.com>
2024-04-05 20:29:50 +0700

    feat: fix duplicate

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,3 @@ int Pemain::beratWin = 5;
 int Pemain::uangWin = 1991;
 int Pemain::defaultBerat = 69;
 int Pemain::defaultUang = 100;
-#include "Pemain.hpp"
-
-int Pemain::beratWin = 5;
-int Pemain::uangWin = 1991;
-int Pemain::defaultBerat = 69;
-int Pemain::defaultUang = 100;

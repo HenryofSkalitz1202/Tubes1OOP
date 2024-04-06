7de7c22c60f91db05dff06614e59ecf6d48ad59d
HenryofSkalitz1202 <edarrell1202@gmail.com>
2024-04-06 12:53:44 +0700

    feat: add Controller

---
+++ src/Controller.hpp
@@ -0,0 +1,63 @@
+#ifndef CONTROLLER_HPP
+#define CONTROLLER_HPP
+
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include "Pemain.hpp"
+using namespace std;
+
+class Controller
+{
+private:
+    std::vector<Pemain*> players;
+    size_t current_player_index;
+    Pemain* current_player;
+    int turn_number;
+    bool game_over;
+
+public:
+    Controller();
+    ~Controller();
+
+    void set_current_player(Pemain* player);
+    Pemain* get_current_player();
+    void print_current_player();
+
+    void sort_players();
+    void add_player(Pemain* player);
+    Pemain* get_first_player();
+    void print_players();
+
+    bool is_game_over();
+    void is_won();
+
+    void start_default();
+    void start_load();
+
+    bool is_petani(Pemain* player);
+    bool is_peternak(Pemain* player);
+    bool is_walikota(Pemain* player);
+
+    void next();
+    void cetak_penyimpanan();
+    void pungut_pajak();
+    void cetak_ladang();
+    void cetak_peternakan();
+    void tanam();
+    void ternak();
+    void bangun();
+    void makan();
+    void kasih_makan();
+    void beli();
+    void jual();
+    void panen();
+    void muat();
+    void simpan();
+    void tambah_pemain();
+
+};
+
+#endif

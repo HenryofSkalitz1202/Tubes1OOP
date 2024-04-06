326cf40cd5d526321cf270b513c698720c7e7a29
HenryofSkalitz1202 <edarrell1202@gmail.com>
2024-04-06 12:46:37 +0700

    feat: update Bangunan

---
+++ src/Bangunan.cpp
@@ -0,0 +1,47 @@
+#include "Bangunan.hpp"
+
+Bangunan::Bangunan(int bangunanID, string namaBangunan, int hargaBangunan){
+    this->bangunanID =  bangunanID;
+    this->namaBangunan = namaBangunan;
+    this->hargaBangunan = hargaBangunan;
+}
+
+Bangunan::~Bangunan(){}
+
+Bangunan::Bangunan(Bangunan& other){
+    this->bangunanID = other.bangunanID;
+    this->namaBangunan = other.namaBangunan;
+    this->hargaBangunan = other.hargaBangunan;
+}
+
+Bangunan& Bangunan::operator=(const Bangunan& other){
+    this->bangunanID = other.bangunanID;
+    this->namaBangunan = other.namaBangunan;
+    this->hargaBangunan = other.hargaBangunan;
+}
+
+void Bangunan::setBangunanID(int bangunanID){
+    this->bangunanID = bangunanID;
+}
+
+void Bangunan::setNamaBangunan(string namaBangunan){
+    this->namaBangunan = namaBangunan;
+}
+
+void Bangunan::setHargaBangunan(int hargaBangunan){
+    this->hargaBangunan = hargaBangunan;
+}
+
+int Bangunan::getBangunanID(){
+    return this->bangunanID;
+}
+
+string Bangunan::getNamaBangunan(){
+    return this->namaBangunan;
+}
+
+int Bangunan::getHargaBangunan(){
+    return this->hargaBangunan;
+}
+
+//void Bangunan::print(){ //TO DO}

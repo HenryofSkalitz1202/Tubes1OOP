3323b21b1e3b078f2a1eac03005c2258d60f0cdd
HenryofSkalitz1202 <edarrell1202@gmail.com>
2024-04-08 20:32:36 +0700

    feat: add Asset

---
+++ src/Asset.hpp
@@ -0,0 +1,19 @@
+#ifndef ASSET_HPP
+#define ASSET_HPP
+#include <string>
+
+using namespace std;
+
+class Asset{
+protected:
+  string assetType;
+public:
+  ~Asset(){}
+
+  virtual void setAssetType() = 0;
+  string getAssetType(){
+    return this->assetType;
+  }
+};
+
+#endif

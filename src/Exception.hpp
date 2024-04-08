1622fbd5085b6b37095a364d8583551a4fe47f00
HenryofSkalitz1202 <edarrell1202@gmail.com>
2024-04-08 11:34:14 +0700

    feat: add Exception

---
+++ src/Exception.hpp
@@ -0,0 +1,25 @@
+#ifndef CUSTOM_EXCEPTION_HPP
+#define CUSTOM_EXCEPTION_HPP
+
+#include <exception>
+using namespace std;
+
+struct NotHerbivoreException : public exception{
+    const char* what() const throw(){
+        return " bukan herbivore";
+    }
+};
+
+struct NotCarnivoreException : public exception{
+    const char* what() const throw(){
+        return " bukan carnivore";
+    }
+};
+
+struct NotOmnivoreException : public exception{
+    const char* what() const throw(){
+        return " bukan omnivore";
+    }
+};
+
+#endif
